df4dc91ba18c417bcfec9fe185b336bc26f0e688
Joshua Okoro <josidbobo@gmail.com>
2022-05-30 19:16:24 +0100

    Update MyVotingApp.sol

--- MyVotingApp.sol
+++ MyVotingApp.sol
@@ -9,7 +9,6 @@ import "./IVotingContract.sol";
 contract MyVotingApp is IVotingContract{
     struct Voter {
         bool hasVoted;  // if true, that person already voted
-        uint vote;   // index of the voted candidate
     }
 
      struct Candidate {
@@ -20,7 +19,35 @@ contract MyVotingApp is IVotingContract{
         //uint candidateId;
     }
 
+    modifier onlyChairperson(){
+        require(msg.sender == chairperson, "Only Chairperson is allowed to add candidates");
+        _;
+    }
+    
+    // Voting time has elapsed
+    modifier votingEnded(){
+        require(block.timestamp < deployedTime + 360, "Voting has ended");
+        _;
+    }
+    
+    // Check that voter has not voted before
+    modifier alreadyVoted(){
+        require (voters[msg.sender].hasVoted == false, "You have already voted");
+        _;
+    }
+    
+    // Prevent adding the same candidate twice
+    modifier noDuplicates(bytes32 _candidate){
+        for(uint i = 0; i < candidates.length; i++){
+            if(candidates[i].name == (_candidate)){
+                revert ("Candidate already exists");
+            }
+        }
+        _;
+    }
+
     uint deployedTime;
+
     address public chairperson;
 
     mapping(address => Voter) public voters;
@@ -33,28 +60,28 @@ contract MyVotingApp is IVotingContract{
         deployedTime = block.timestamp;
     }
 
-    function addCandidate(bytes32 candidate)  external override  returns(bool) {
-        require (msg.sender == chairperson, "Only Chairperson is allowed to add candidates");
+    function changeChairperson(address _chairperson) public onlyChairperson returns(bool)  {
+        chairperson = _chairperson;
+        return true;
+    }
+
+    function addCandidate(bytes32 candidate)  external override onlyChairperson noDuplicates(candidate) returns(bool) {
         require (block.timestamp < deployedTime + 180, "Window to add candidate has passed");
         candidates.push(Candidate({
             name:candidate,
             voteCount:0
         }));
-    return true;
-}
+        return true;
+    }
 
+    function voteCandidate(uint candidateId) external override votingEnded alreadyVoted returns(bool) {
+        require (block.timestamp > deployedTime + 180, "Kindly wait, candidates are being added.");
+        candidates[candidateId].voteCount += 1;
+        voters[msg.sender].hasVoted = true;
+        return true;
+    }
 
-
-function voteCandidate(uint candidateId) external override returns(bool){
-    require (block.timestamp > deployedTime + 180, "Kindly wait, candidates are being added.");
-    require (block.timestamp < deployedTime + 360, "Voting has ended");
-    require (voters[msg.sender].hasVoted == false, "You have already voted");
-    candidates[candidateId].voteCount += 1;
-    voters[msg.sender].hasVoted = true;
-    return true;
-}
-
-//getWinner returns the name of the winner
+    //getWinner returns the name of the winner
     function getWinner() external override view returns(bytes32){
         uint initialHigh = 0;
         for(uint index = 0; index < candidates.length; index++){
@@ -63,15 +90,14 @@ function voteCandidate(uint candidateId) external override returns(bool){
             } 
         }
         return candidates[initialHigh].name;
-        //return initialHigh;
-        //return "";
-    }
-}
 
-/*
-[
-     "0x4d75626172617100000000000000000000000000000000000000000000000000",
- "0x4f6f6b616e000000000000000000000000000000000000000000000000000000",
- "0x4c6177616c000000000000000000000000000000000000000000000000000000"
- ]
-*/
+        /* 
+        The code to convert to string is marked in arrow, but I don't think it's necessary cos of gas fees 
+        and we would have to alter the IVoting interface cos the return types differ.
+
+        ///  --->  string memory convt_bytes = string(candidates[initialHigh].name);
+                    return convt_bytes;
+
+        */
+            }
+        }
