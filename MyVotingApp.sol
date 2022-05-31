f916febbdd485a4929e08fa3c03534551b978b1d
LAWAL Mubaraq Ayobami <88977598+OokanNumber1@users.noreply.github.com>
2022-05-31 18:32:20 +0100

    Update MyVotingApp.sol

--- MyVotingApp.sol
+++ MyVotingApp.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.7;
+pragma solidity = 0.8.7;
 
 //import "https://github.com/Ekolance/Voting-Smart-Contract-Interface/blob/main/IVotingContract.sol";
 import "./IVotingContract.sol";
